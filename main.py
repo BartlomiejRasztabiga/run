edd1c8c998cb0722c2b8b3de5a34e0ea0b7351ce
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-06-18 12:50:30 +0200

    Running on k8s?

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uuid
 import shutil
 import docker
 from contextlib import asynccontextmanager
+import kubernetes
 
 from fastapi import FastAPI
 from git import Repo
@@ -11,6 +12,11 @@ from pydantic import BaseModel
 BASE_PATH = "./temp"
 
 docker_client = docker.from_env()
+
+kubernetes.config.load_kube_config()
+k8s_core_api_client = kubernetes.client.CoreV1Api()
+k8s_apps_api_client = kubernetes.client.AppsV1Api()
+k8s_networking_api_client = kubernetes.client.NetworkingV1Api()
 
 
 class CreateDeploymentRequest(BaseModel):
@@ -60,29 +66,103 @@ async def push_image(image_tag):
     print(new_image_tag)
     docker_client.images.get(image_tag).tag(new_image_tag)
     docker_client.images.push(new_image_tag)
+    return new_image_tag
+
+
+async def create_k8s_deployment(namespace, image_tag, deployment_name):
+    k8s_core_api_client.create_namespace(namespace)
+
+    # TODO jaki port???
+
+    container = kubernetes.client.V1Container(
+        name="container",
+        image=image_tag,
+        ports=[kubernetes.client.V1ContainerPort(container_port=80)]
+    )
+
+    # Create and configure a spec section
+    template = kubernetes.client.V1PodTemplateSpec(
+        metadata=kubernetes.client.V1ObjectMeta(labels={"app": "container"}),
+        spec=kubernetes.client.V1PodSpec(containers=[container]),
+    )
+
+    # Create the specification of deployment
+    spec = kubernetes.client.V1DeploymentSpec(
+        replicas=1, template=template, selector={
+            "matchLabels":
+                {"app": "container"}})
+
+    # Instantiate the deployment object
+    deployment = kubernetes.client.V1Deployment(
+        api_version="apps/v1",
+        kind="Deployment",
+        metadata=kubernetes.client.V1ObjectMeta(name=deployment_name),
+        spec=spec,
+    )
+
+    k8s_apps_api_client.create_namespaced_deployment(namespace, deployment)
+
+    # create service
+    service = kubernetes.client.V1Service(
+        api_version="v1",
+        kind="Service",
+        metadata=kubernetes.client.V1ObjectMeta(name=deployment_name),
+        spec=kubernetes.client.V1ServiceSpec(
+            selector={"app": "container"},
+            ports=[kubernetes.client.V1ServicePort(
+                port=80, target_port=80)]
+        )
+    )
+    k8s_core_api_client.create_namespaced_service(namespace, service)
+
+    # create ingress
+    ingress = kubernetes.client.V1Ingress(
+        api_version="networking.k8s.io/v1",
+        kind="Ingress",
+        metadata=kubernetes.client.V1ObjectMeta(name=deployment_name),
+        spec=kubernetes.client.V1IngressSpec(
+            rules=[kubernetes.client.V1IngressRule(
+                host=f"{deployment_name}.rasztabiga.me",
+                http=kubernetes.client.V1HTTPIngressRuleValue(
+                    paths=[kubernetes.client.V1HTTPIngressPath(
+                        path="/",
+                        backend=kubernetes.client.V1IngressServiceBackend(
+                            port=80,
+                            name=deployment_name
+                        )
+                    )]
+                )
+            )]
+        )
+    )
+    k8s_networking_api_client.create_namespaced_ingress(namespace, ingress)
+
+    # get url
+    return f"https://{deployment_name}.rasztabiga.me"
 
 
 async def deploy(repo_url):
     repo_name = repo_url.split("/")[-1].split(".")[0]
     user_id = uuid.uuid4()
-    image_name = f"{repo_name}-{user_id}"
-    repo_path = f"{BASE_PATH}/{image_name}"
+    deployment_id = f"{repo_name}-{user_id}"
+    repo_path = f"{BASE_PATH}/{deployment_id}"
 
     await clone_repo(repo_url, repo_path)
     await check_dockerfile(repo_path)
-    image_tag = await build_image(repo_path, image_name)
+    image_tag = await build_image(repo_path, deployment_id)
     print(image_tag)
 
-    await push_image(image_tag)
+    new_image_tag = await push_image(image_tag)
     print("pushed")
 
+    url = await create_k8s_deployment(deployment_id, new_image_tag, deployment_id)
 
     # push docker image to cluster?
     # create k8s deployment + service + ingress config giles
     # apply k8s config files
     # return url
 
-    return image_tag
+    return url
 
 
 @app.get("/")
@@ -97,5 +177,5 @@ async def say_hello(name: str):
 
 @app.post("/deployments")
 async def create_deployment(request: CreateDeploymentRequest):
-    image_tag = await deploy(request.repo_url)
-    return {"image_tag": image_tag}
+    url = await deploy(request.repo_url)
+    return {"url": url}
