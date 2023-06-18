f6a13851ca4c26504b2be840f74bd5f5e7b2546d
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2023-06-18 12:56:39 +0200

    Running on k8s?

--- main.py
+++ main.py
@@ -137,6 +137,7 @@ async def create_k8s_deployment(namespace_name, image_tag, deployment_name):
                 http=kubernetes.client.V1HTTPIngressRuleValue(
                     paths=[kubernetes.client.V1HTTPIngressPath(
                         path="/",
+                        path_type="Prefix",
                         backend=kubernetes.client.V1IngressServiceBackend(
                             port=80,
                             name=deployment_name
