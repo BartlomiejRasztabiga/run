f65b6e2af6de4b9113c73a1f1771e304a1b41791
Bartłomiej Rasztabiga <bartlomiej.rasztabiga@symmetrical.ai>
2024-08-20 21:46:37 +0200

    WIP trying to make it work with EM

--- chatgpt.py
+++ chatgpt.py
@@ -1,11 +1,27 @@
 import os
 import docker
+import logging
 
 from dotenv import load_dotenv
 from openai import OpenAI
 from git import Repo
 
-def tree(some_dir, level):
+load_dotenv()
+
+logger = logging.getLogger(__name__)
+
+
+def prepare_working_directory(tmp_dir):
+    logger.info("Preparing working directory...")
+
+    # clear dir
+    os.system(f"rm -rf {tmp_dir}")
+
+    # create dir
+    os.makedirs(tmp_dir, exist_ok=True)
+
+
+def tree(some_dir, level, ignore):
     some_dir = some_dir.rstrip(os.path.sep)
     assert os.path.isdir(some_dir)
     num_sep = some_dir.count(os.path.sep)
@@ -14,127 +30,141 @@ def tree(some_dir, level):
         num_sep_this = root.count(os.path.sep)
         if num_sep + level <= num_sep_this:
             del dirs[:]
+        for i in ignore:
+            if i in dirs:
+                dirs.remove(i)
 
-load_dotenv()
 
-client = OpenAI()
+def tree_to_str(tree, trim_dir=None):
+    tree_str = ""
+    for root, dirs, files in tree:
+        if trim_dir:
+            root = root.replace(trim_dir, "")
+        for file in files:
+            tree_str += f"{root}/{file}\n"
+    return tree_str
 
-# model = "gpt-4o-mini"
-model = "gpt-4o"
-temperature = 0.2
 
-# repo_url = "https://github.com/BartlomiejRasztabiga/run-example.git"
-repo_url = "git@bitbucket.org:symmetricalai/employee-management.git"
-repo_name = repo_url.split("/")[-1].replace(".git", "")
+def do_magic(repo_url):
+    logger.info("Starting with repo_url: %s", repo_url)
 
-tmp_dir = f"./tmp/{repo_name}"
+    client = OpenAI()
 
-print("Preparing working directory...")
+    model = "gpt-4o-mini"
+    # model = "gpt-4o"
+    temperature = 0.2
 
-# clear dir
-os.system(f"rm -rf {tmp_dir}")
+    repo_name = repo_url.split("/")[-1].replace(".git", "")
 
-# create dir
-os.makedirs(tmp_dir, exist_ok=True)
+    tmp_dir = f"./tmp/{repo_name}"
 
-print("Cloning repository...")
+    prepare_working_directory(tmp_dir)
 
-# clone repo
-repo = Repo.clone_from(repo_url, tmp_dir)
+    logger.info("Cloning repository...")
 
-print("Preparing tree...")
+    # clone repo
+    Repo.clone_from(repo_url, tmp_dir)
 
-# get tree ignoring .git
-tree = tree(tmp_dir, level=1)
+    logger.info("Preparing tree...")
 
-# tree to string, ignoring .git
-tree_str = ""
-for root, dirs, files in tree:
-    root_without_tmp = root.replace(tmp_dir, "")
+    # get tree ignoring .git
+    dir_tree = tree(tmp_dir, level=1, ignore=[".git"])
+
+    # tree to string, ignoring .git
+    tree_str = tree_to_str(dir_tree, trim_dir=tmp_dir)
+
+    # logger.info(tree_str)
+
+    logger.info("Finding important files...")
+
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant, that given repository files structure (only some part of it) will help to identify the most important files to generate a Dockerfile to build a valid docker image that can be run to run the app of repository. Respond only with the file names, in the same format as provided, ignore formatting markers.",
+            },
+            {"role": "user", "content": tree_str},
+        ],
+        temperature=temperature,
+    )
+
+    content = completion.choices[0].message.content
+
+    logger.info("Preparing files content...")
+
+    # get files from response and trim (strip)
+    files = list(map(lambda x: x.strip(), content.split("\n")))
+
+    # ignore .jar and Dockerfile files (unsupported)
+    ignored_files = [".jar", "Dockerfile"]
+    files = [file for file in files if not any(ignored_file in file for ignored_file in ignored_files)]
+
+    # get files content
+    files_content = {}
     for file in files:
-        if ".git" not in root:
-            tree_str += f"{root_without_tmp}/{file}\n"
+        with open(tmp_dir + file, "r") as f:
+            files_content[file] = f.read()
 
-# print(tree_str)
+    logger.info("Generating Dockerfile...")
 
-print("Finding important files...")
+    prompt = f"""
+    {tree_str}
+    
+    {files_content}
+    """
 
-completion = client.chat.completions.create(
-    model=model,
-    messages=[
-        {
-            "role": "system",
-            "content": "You are a helpful assistant, that given repository files structure (only some part of it) will help to identify the most important files to generate a Dockerfile to build a valid docker image that can be run to run the app of repository. Respond only with the file names, in the same format as provided, ignore formatting markers.",
-        },
-        {"role": "user", "content": tree_str},
-    ],
-    temperature=temperature,
-)
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant, that given repository files structure (only some part of it) and content of the most important files will help to generate a Dockerfile to build a valid docker image that can be run to run the app of repository. Use latest base image versions and best practises, implement all security measures and expose all necessary ports. Respond only with the content of the Dockerfile, ignore formatting markers.",
+            },
+            {"role": "user", "content": prompt},
+        ],
+        temperature=temperature,
+    )
 
-content = completion.choices[0].message.content
+    content = completion.choices[0].message.content
 
-print("Preparing files content...")
+    logger.info("Writing Dockerfile...")
 
-# get files from response and trim (strip)
-files = list(map(lambda x: x.strip(), content.split("\n")))
+    # write Dockerfile to tmp
+    with open(tmp_dir + "/Dockerfile", "w") as f:
+        f.write(content)
 
-# ignore .jar and Dockerfile files (unsupported)
-ignored_files = [".jar", "Dockerfile"]
-files = [file for file in files if not any(ignored_file in file for ignored_file in ignored_files)]
+    exposed_ports = []
+    for line in content.split("\n"):
+        if "EXPOSE" in line:
+            exposed_ports = line.split(" ")[1:]
 
-# get files content
-files_content = {}
-for file in files:
-    with open(tmp_dir + file, "r") as f:
-        files_content[file] = f.read()
+    logger.info("Building Docker image...")
 
-print("Generating Dockerfile...")
+    # build docker image
+    client = docker.from_env()
 
-prompt = f"""
-{tree_str}
+    image_tag = repo_name.lower()
+    image, logs = client.images.build(path=tmp_dir, tag=image_tag)
 
-{files_content}
-"""
+    logger.info("Running Docker image...")
 
-completion = client.chat.completions.create(
-    model=model,
-    messages=[
-        {
-            "role": "system",
-            "content": "You are a helpful assistant, that given repository files structure (only some part of it) and content of the most important files will help to generate a Dockerfile to build a valid docker image that can be run to run the app of repository. Use latest base image versions and best practises, implement all security measures and expose all necessary ports. Respond only with the content of the Dockerfile, ignore formatting markers.",
-        },
-        {"role": "user", "content": prompt},
-    ],
-    temperature=temperature,
-)
+    # run docker image, expose ports according to Dockerfile
+    ports = {}
+    for port in exposed_ports:
+        ports[port] = None
 
-content = completion.choices[0].message.content
+    container = client.containers.run(image, detach=True, ports=ports)
 
-print("Writing Dockerfile...")
+    logger.info("DONE")
 
-# write Dockerfile to tmp
-with open(tmp_dir + "/Dockerfile", "w") as f:
-    f.write(content)
+def main():
+    logging.basicConfig(level=logging.INFO)
 
-exposed_ports = []
-for line in content.split("\n"):
-    if "EXPOSE" in line:
-        exposed_ports = line.split(" ")[1:]
+    # repo_url = "https://github.com/BartlomiejRasztabiga/run-example.git"
+    # repo_url = "git@bitbucket.org:symmetricalai/employee-management.git"
+    repo_url = "git@github.com:BartlomiejRasztabiga/FO23Z.git"
+    do_magic(repo_url)
 
-print("Building Docker image...")
-
-# build docker image
-client = docker.from_env()
-
-image, logs = client.images.build(path=tmp_dir, tag=repo_name)
-
-print("Running Docker image...")
-
-# run docker image, expose ports according to Dockerfile
-ports = {}
-for port in exposed_ports:
-    ports[port] = None
-
-container = client.containers.run(image, detach=True, ports=ports)
-
-print("DONE")
+if __name__ == "__main__":
+    main()
